d76f81e2e0dfadafd73e507c5cb04c7337d667b3
castillorobin <robin.castillo@catolica.edu.sv>
2023-07-03 10:53:18 -0600

    cambios en Ver factura

--- resources/views/factura/inicio.blade.php
+++ resources/views/factura/inicio.blade.php
@@ -175,6 +175,7 @@ jQuery(document).ready(function($){
                     <td>{{ $pedidos[$i]->tipo }}</td>  
                     <td>{{ $pedidos[$i]->estado }} </td>
                     <td>{{ $pedidos[$i]->fecha_entrega }} </td>
+
                     @if($pedidos[$i]->pagado=='Pagado')
                     <td class="text-center"><h5><span class="badge badge-success ">{{ $pedidos[$i]->pagado }} </span></h5></td>
                     @else
@@ -189,7 +190,26 @@ jQuery(document).ready(function($){
                     <td>{{ $pedidos[$i]->total }} </td>
                     <td>{{ $pedidos[$i]->agencia }} </td>
                     
-
+    <span hidden id="nom{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->vendedor }}</span>
+    <span hidden id="des{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->destinatario }}</span>
+    <span hidden id="tel{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->telefono }}</span>
+    <span hidden id="dir{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->direccion}}</span>
+    <span hidden id="fec{{ $pedidos[$i]->id }}"> {{  date('d/m/Y', strtotime($pedidos[$i]->created_at))  }}</span>
+    <span hidden id="fece{{ $pedidos[$i]->id }}"> {{  date('d/m/Y', strtotime($pedidos[$i]->fecha_entrega))}}</span>
+    <span hidden id="tip{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->tipo}}</span>
+    <span hidden id="este{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->estado}}</span>
+    <span hidden id="estp{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->pagado}}</span>
+    <span hidden id="pre{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->precio}}</span>
+    <span hidden id="env{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->envio}}</span>
+    <span hidden id="tot{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->total}}</span>
+    <span hidden id="ing{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->ingresado}}</span>
+    <span hidden id="ang{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->agencia}}</span>
+    <span hidden id="rep{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->repartidor}}</span>
+    <span hidden id="rut{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->ruta}}</span>
+    <span hidden id="not{{ $pedidos[$i]->id }}"> {{ $pedidos[$i]->nota}}</span>
+    <span hidden id="fot{{ $pedidos[$i]->id }}"> /imgs/fotos/{{ $pedidos[$i]->foto}}</span>
+    <span hidden id="fot2{{ $pedidos[$i]->id }}"> /imgs/fotos/{{ $pedidos[$i]->foto2}}</span>
+    <span hidden id="fot3{{ $pedidos[$i]->id }}"> /imgs/fotos/{{ $pedidos[$i]->foto3}}</span>
 
 
                     <td class="opciones text-center" style="">
@@ -213,7 +233,7 @@ jQuery(document).ready(function($){
     &nbsp;
     <i class="fas fa-eye"></i>
     &nbsp;&nbsp;
-    <button type="button" class="edit" data-toggle="modal" value="{{ $pedidos[$i]->id }}" data-target="#exampleModal" style="background: none; border: 0;">Ver</button>
+    <button type="button" class="edit" data-toggle="modal" value="{{ $pedidos[$i]->id }}" data-target="#exampleModal2" style="background: none; border: 0;">Ver</button>
 </form>
 </li>
 <li class="botones">
@@ -467,7 +487,7 @@ Nota de descuento
 
 <br>
 <div class="col-12 mb-3">
-<a href="/factura/facturapdf" ><button type="button" class="btn btn-lg btn-warning btn-block">PAGAR</button>
+<a href="/factura/facturapdf" ><button type="button" class="btn btn-lg btn-warning btn-block">PAGAR</button> </a>
 </div>
 
 </div> <!-- Termina cajero, pagos etc.  -->
@@ -489,6 +509,165 @@ Nota de descuento
 
 
 
+
+
+
+
+
+
+
+
+<!-- Inicio Modal Ver-->
+
+<div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel" style="float: left;"></h1> &nbsp; &nbsp; &nbsp;
+        <span style="float: right; text-align: right;"><button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">X</button></span>
+      </div>
+      <div class="modal-body">
+        <div class="row p-3 m-3" style="border: solid 1px;">
+           
+            <table class="table table-borderless" >
+                <tr>
+                    <td  colspan="3"><h4>Datos del Comercio</h4>
+                        <hr>
+                    </td>
+                    
+                </tr>
+
+                <tr class="headt">
+                    <td width="230px">Nombre de comercio / Tienda </td>
+                    <td> <span ></span> <label for="" id="nombre"></label> </td>
+                    <td rowspan="15"><span ></span> <label for="" > </label> <img alt="" class=" img-thumbnail" id="fotos" width="250"> 
+                    <br> <img alt="" id="fotos2" width="250">
+                    <br> <img alt="" id="fotos3" width="250"></td>
+                </tr>
+
+                <tr class="headt">
+                    <td width="230px">Dirección </td>
+                    <td> <span ></span> <label for="" id="dire"></label> </td>
+                    
+                </tr>
+                
+
+                <tr class="headt">
+                    <br>
+                    <td  colspan="3" class="pt-2"> <h4> Datos del destinatario</h4>
+                        <hr>
+                    </td>
+                      
+                </tr>
+
+                <tr class="headt">
+                    <td width="230px">Destinatario </td>
+                    <td> <span ></span> <label for="" id="desti"></label> </td>
+                    
+                </tr>
+                <tr class="headt">
+                    <td width="230px">Telefono </td>
+                    <td> <span ></span> <label for="" id="telef"></label> </td>
+                    
+                </tr>
+                <tr class="headt">
+                    <td width="230px">Direccion de entrega </td>
+                    <td> <span ></span> <label for="" id="direc"></label> </td>
+                    
+                </tr>
+                
+
+                <tr class="headt">
+                    <br>
+                    <td  colspan="3" class="pt-2"> <h4> Datos del paquete</h4>
+                        <hr>
+                    </td>
+                      
+                </tr>
+
+                <tr class="headt">
+                    <td width="230px">Fecha de creacion </td>
+                    <td> <span ></span> <label for="" id="fecha"></label> </td>  
+                </tr>
+                <tr class="headt">
+                    <td width="230px">Fecha de entrega </td>
+                    <td> <span ></span> <label for="" id="fechen"></label> </td>  
+                </tr>
+                <tr class="headt">
+                    <td width="230px">Tipo de envio </td>
+                    <td> <span ></span> <label for="" id="tipoe"></label> </td>  
+                </tr>
+                </tr>
+                <tr class="headt">
+                    <td width="230px">Estado del envio </td>
+                    <td> <span ></span> <label for="" id="este"></label> </td>  
+                </tr>
+                <tr class="headt">
+                    <td width="230px">Estado del pago </td>
+                    <td> <span ></span> <label for="" id="estp"></label> </td>  
+                </tr>
+                <tr class="headt">
+                    <td width="230px">Precio del paquete </td>
+                    <td> <span ></span> <label for="" id="preci"></label> </td>  
+                </tr>
+                <tr class="headt">
+                    <td width="230px">Costo del envio </td>
+                    <td> <span ></span> <label for="" id="envio"></label> </td>  
+                </tr>
+                <tr class="headt">
+                    <td width="230px">Total a cobrar </td>
+                    <td> <span ></span> <label for="" id="total"></label> </td>  
+                </tr>
+
+                <tr class="headt">
+                    <br>
+                    <td  colspan="3" class="pt-2"> <h4> Datos internos</h4>
+                        <hr>
+                    </td>
+                      
+                </tr>
+
+                <tr class="headt">
+                    <td width="230px">Usuario que registró </td>
+                    <td> <span ></span> <label for="" id="ingre"></label> </td>  
+                </tr>
+                <tr class="headt">
+                    <td width="230px">Recepción agencia </td>
+                    <td> <span ></span> <label for="" id="agen"></label> </td>  
+                </tr>
+                <tr class="headt">
+                    <td width="230px">Repartidor </td>
+                    <td> <span ></span> <label for="" id="repar"></label> </td>  
+                </tr>
+                <tr class="headt">
+                    <td width="230px">Ruta </td>
+                    <td> <span ></span> <label for="" id="ruta1"></label> </td>  
+                </tr>
+                <tr class="headt">
+                    <td width="230px">Nota </td>
+                    <td> <span ></span> <label for="" id="nota1"></label> </td>  
+                </tr>
+
+
+            </table>
+
+
+        </div>
+
+
+
+       
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-times" style="color: #ffffff;"></i> &nbsp; Cerrar</button>
+        <a id="impri" class="btn btn-primary" style="color: #ffffff;">Imprimir</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- Termina Modal ver-->
 
 
 
@@ -569,4 +748,78 @@ document.getElementById("preci2").textContent = final;
 
 
         </script> 
+        
+<script>
+       
+        
+       $(document).ready(function(){
+           $(document).on('click', '.edit', function(){
+               var id=$(this).val();
+               var nomb=$('#nom'+id).text();
+               var dest=$('#des'+id).text();
+               var tele=$('#tel'+id).text();
+               var dire=$('#dir'+id).text();
+               var fech=$('#fec'+id).text();
+               var feche=$('#fece'+id).text();
+               var tipo=$('#tip'+id).text();
+               var est=$('#este'+id).text();
+               var esp=$('#estp'+id).text();
+               var prec=$('#pre'+id).text();
+               var envi=$('#env'+id).text();
+               var tota=$('#tot'+id).text();
+               var ingr=$('#ing'+id).text();
+               var ange=$('#ang'+id).text();
+               var repa=$('#rep'+id).text();
+               var ruta=$('#rut'+id).text();
+               var nota=$('#not'+id).text();
+               var foto=$('#fot'+id).text();
+               var foto2=$('#fot2'+id).text();
+               var foto3=$('#fot3'+id).text();
+               //foti= '/imgs/fotos/';
+       
+               
+           
+               $('#edit').modal('show');
+               $('#nombre').text(nomb);
+               $('#desti').text(dest);
+               $('#telef').text(tele);
+               $('#direc').text(dire);
+               $('#fecha').text(fech);
+               $('#fechen').text(feche);
+               $('#tipoe').text(tipo);
+               $('#este').text(est);
+               $('#estp').text(esp);
+               $('#preci').text(prec);  
+               $('#envio').text(envi);
+               $('#total').text(tota);
+               $('#ingre').text(ingr);
+               $('#agen').text(ange);
+               $('#repar').text(repa);
+               $('#ruta1').text(ruta);
+               $('#nota1').text(nota);
+       
+               $('#empresa').text(empre);
+               $('#giro').text(gir);   
+        
+               $('#nrc').text(nr);
+               //$('#fotos').src(fot);
+               var ide = '/repartidor/imprimir/'+id ;
+               $('#fotos').attr("src", foto);
+               $('#fotos2').attr("src", foto2);
+               $('#fotos3').attr("src", foto3);
+       
+               //$('#impri a').prop("href", ide);
+               //$('.paginacion a').prop('href','http://nuevaUrl.com');
+       
+               document.getElementById("impri").href = ide;
+           });
+       });
+        
+       
+       
+       
+       
+       
+       
+           </script>
 @endsection
