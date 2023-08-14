8f977110c188aeb565fd7e7d18d20156ed70637a
castillorobin <robin.castillo@catolica.edu.sv>
2023-08-14 16:28:58 -0600

    cambios en camara

--- resources/views/pedido/create.blade.php
+++ resources/views/pedido/create.blade.php
@@ -19,8 +19,6 @@ function mostrando()
   //inicia archivo webcam
 
   
-
-
   // finalizo archivo webcam
 
 
@@ -777,25 +775,8 @@ $(document).ready(function() {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.3.3/jquery.inputmask.bundle.min.js"></script>
 
 <script>
- /*
- let camera_button = document.querySelector("#start-camera");
-let video = document.querySelector("#video");
-let click_button = document.querySelector("#click-photo");
-let canvas = document.querySelector("#canvas");
 
-camera_button.addEventListener('click', async function() {
-   	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-	video.srcObject = stream;
-});
 
-click_button.addEventListener('click', function() {
-   	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-   	let image_data_url = canvas.toDataURL('image/jpeg');
-
-   	// data url of the image
-   	console.log(image_data_url);
-});
-*/
 
 Webcam.set({
         width: 490,
