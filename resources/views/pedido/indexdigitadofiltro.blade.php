0015944e00829f4675b5afc3b62fc43234fd451c
castillorobin <robin.castillo@catolica.edu.sv>
2023-12-15 11:38:33 -0600

    otros cambios digi

--- resources/views/pedido/indexdigitadofiltro.blade.php
+++ resources/views/pedido/indexdigitadofiltro.blade.php
@@ -195,8 +195,8 @@ jQuery(document).ready(function($){
     //console.log(data.text);
     //document.getElementById('mostrar').value = data.text;
     //window.location = "https://appmeloexpress.com/pedido/indexdigitadofiltro/" + data.text; 
-    //window.location = "http://209.145.56.57/pedido/indexdigitadofiltro/" + data.text;
-    window.location = "http://127.0.0.1:8000/pedido/indexdigitadofiltro/" + data.text;
+    window.location = "http://209.145.56.57/pedido/indexdigitadofiltro/" + data.text;
+    //window.location = "http://127.0.0.1:8000/pedido/indexdigitadofiltro/" + data.text;
         });
 
     });
